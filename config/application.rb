f8170089109af8ae41fe89302072c01d8a8326b6
aliahmadexe <aliahmad@sedintechnologies.com>
2023-11-10 00:36:22 +0500

    deleted client querys

--- config/application.rb
+++ config/application.rb
@@ -30,5 +30,6 @@ module ProfileUpdate
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.autoloader = :classic
   end
 end
