ad3699c48931e19ca504d4c437c8435683761d0e
aliahmadexe <aliahmad@sedintechnologies.com>
2023-11-10 00:26:43 +0500

    deleted client query

--- app/models/client_query.rb
+++
@@ -1,4 +0,0 @@
-# class ClientQuery < ApplicationRecord
-#   validates :name, :email, :subject, :message, presence: true
-#   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP, message: "must be a valid email address" }
-# end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,3 @@
 Rails.application.routes.draw do
   root 'home#index_main'
-  get 'home/index'
-  post 'home/client_query'
 end
